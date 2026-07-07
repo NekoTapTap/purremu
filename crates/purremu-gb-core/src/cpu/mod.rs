da6d7fa11d98e39c50275525add24e51dbf713e4
LemonNeko <self@lemonneko.moe>
2026-07-07 17:58:04 +0800

    feat: implement `add a, imm8` and `add a, r`

--- crates/purremu-gb-core/src/cpu/mod.rs
+++ crates/purremu-gb-core/src/cpu/mod.rs
@@ -1,5 +1,6 @@
 use crate::memory_bus::MemoryBus;
 
+#[cfg(test)]
 mod tests;
 
 #[derive(PartialEq, Debug, Clone, Copy)]
@@ -23,33 +24,145 @@ pub enum CpuRegister {
 #[derive(PartialEq, Debug, Clone, Copy)]
 pub enum CpuInstruction {
     LdRImm8(CpuRegister),
+    AddAImm8,
+    AddAR(CpuRegister),
 }
 
 impl From<u8> for CpuInstruction {
     fn from(opcode: u8) -> Self {
         match opcode {
-            0x3E => CpuInstruction::LdRImm8(CpuRegister::A), // LD A, d8
-            0x06 => CpuInstruction::LdRImm8(CpuRegister::B), // LD B, d8
-            0x0E => CpuInstruction::LdRImm8(CpuRegister::C), // LD C, d8
-            0x16 => CpuInstruction::LdRImm8(CpuRegister::D), // LD D, d8
-            0x1E => CpuInstruction::LdRImm8(CpuRegister::E), // LD E, d8
-            0x26 => CpuInstruction::LdRImm8(CpuRegister::H), // LD H, d8
-            0x2E => CpuInstruction::LdRImm8(CpuRegister::L), // LD L, d8
+            0x3E => CpuInstruction::LdRImm8(CpuRegister::A), // LD A, imm8
+            0x06 => CpuInstruction::LdRImm8(CpuRegister::B), // LD B, imm8
+            0x0E => CpuInstruction::LdRImm8(CpuRegister::C), // LD C, imm8
+            0x16 => CpuInstruction::LdRImm8(CpuRegister::D), // LD D, imm8
+            0x1E => CpuInstruction::LdRImm8(CpuRegister::E), // LD E, imm8
+            0x26 => CpuInstruction::LdRImm8(CpuRegister::H), // LD H, imm8
+            0x2E => CpuInstruction::LdRImm8(CpuRegister::L), // LD L, imm8
+            0xC6 => CpuInstruction::AddAImm8,                // ADD A, imm8
+            0x80 => CpuInstruction::AddAR(CpuRegister::B), // ADD A, B
+            0x81 => CpuInstruction::AddAR(CpuRegister::C), // ADD A, C
+            0x82 => CpuInstruction::AddAR(CpuRegister::D), // ADD A, D
+            0x83 => CpuInstruction::AddAR(CpuRegister::E), // ADD A, E
+            0x84 => CpuInstruction::AddAR(CpuRegister::H), // ADD A, H
+            0x85 => CpuInstruction::AddAR(CpuRegister::L), // ADD A, L
+            0x87 => CpuInstruction::AddAR(CpuRegister::A), // ADD A, A
             _ => panic!("Opcode {:02X} not implemented", opcode),
         }
     }
 }
 
+#[rustfmt::skip]
 struct CpuRegisters {
     pc: u16,
     sp: u16,
     a: u8,
-    b: u8,
-    c: u8,
-    d: u8,
-    e: u8,
-    h: u8,
-    l: u8,
+    b: u8, c: u8,
+    d: u8, e: u8,
+    h: u8, l: u8,
+    f: CpuFlagsRegister,
+}
+
+impl CpuRegisters {
+    #[rustfmt::skip]
+    fn new() -> Self {
+        Self {
+            pc: 0,
+            sp: 0,
+            a: 0,
+            b: 0, c: 0,
+            d: 0, e: 0,
+            h: 0, l: 0,
+            f: CpuFlagsRegister::new(),
+        }
+    }
+
+    fn set(&mut self, register: CpuRegister, value: u8) {
+        match register {
+            CpuRegister::A => self.a = value,
+            CpuRegister::B => self.b = value,
+            CpuRegister::C => self.c = value,
+            CpuRegister::D => self.d = value,
+            CpuRegister::E => self.e = value,
+            CpuRegister::H => self.h = value,
+            CpuRegister::L => self.l = value,
+        }
+    }
+
+    fn get(&self, register: CpuRegister) -> u8 {
+        match register {
+            CpuRegister::A => self.a,
+            CpuRegister::B => self.b,
+            CpuRegister::C => self.c,
+            CpuRegister::D => self.d,
+            CpuRegister::E => self.e,
+            CpuRegister::H => self.h,
+            CpuRegister::L => self.l,
+        }
+    }
+}
+
+struct CpuFlagsRegister {
+    zero: bool,
+    subtract: bool,
+    half_carry: bool,
+    carry: bool,
+}
+
+impl CpuFlagsRegister {
+    fn new() -> Self {
+        Self {
+            zero: false,
+            subtract: false,
+            half_carry: false,
+            carry: false,
+        }
+    }
+}
+
+impl From<u8> for CpuFlagsRegister {
+    /**
+     * For better understanding, I use binary literals to represent the flags in the 8-bit register.
+     */
+    #[rustfmt::skip]
+    fn from(value: u8) -> Self {
+        Self {
+            zero:       value & 0b1000_0000 != 0,
+            subtract:   value & 0b0100_0000 != 0,
+            half_carry: value & 0b0010_0000 != 0,
+            carry:      value & 0b0001_0000 != 0,
+        }
+    }
+}
+
+impl From<CpuFlagsRegister> for u8 {
+    #[rustfmt::skip]
+    fn from(flags: CpuFlagsRegister) -> Self {
+        (if flags.zero             { 0b1000_0000 } else { 0 })
+            | (if flags.subtract   { 0b0100_0000 } else { 0 })
+            | (if flags.half_carry { 0b0010_0000 } else { 0 })
+            | (if flags.carry      { 0b0001_0000 } else { 0 })
+    }
+}
+
+trait CpuArithmetic
+where
+    Self: Sized,
+{
+    fn cpu_add(&self, value: u8) -> (Self, CpuFlagsRegister);
+}
+
+impl CpuArithmetic for u8 {
+    fn cpu_add(&self, value: u8) -> (Self, CpuFlagsRegister) {
+        let (result, carry) = self.overflowing_add(value);
+
+        let flags = CpuFlagsRegister {
+            zero: result == 0,
+            subtract: false,
+            half_carry: (*self & 0x0F) + (value & 0x0F) > 0x0F,
+            carry: carry,
+        };
+        (result, flags)
+    }
 }
 
 pub struct Cpu {
@@ -63,17 +176,27 @@ impl Cpu {
         Self {
             divider: 0,
             phase: CpuPhase::FetchOpcode,
-            registers: CpuRegisters {
-                pc: 0,
-                sp: 0,
-                a: 0,
-                b: 0,
-                c: 0,
-                d: 0,
-                e: 0,
-                h: 0,
-                l: 0,
-            },
+            registers: CpuRegisters::new(),
+        }
+    }
+
+    fn decode_instruction(&mut self, instruction: CpuInstruction, bus: &MemoryBus) {
+        match instruction {
+            CpuInstruction::LdRImm8(register) => {
+                let value = self.fetch8(bus);
+                self.registers.set(register, value);
+                self.phase = CpuPhase::FetchOpcode;
+            }
+            CpuInstruction::AddAImm8 => {
+                let value = self.fetch8(bus);
+                let (result, flags) = self.registers.a.cpu_add(value);
+                self.registers.a = result;
+                self.registers.f = flags;
+                self.phase = CpuPhase::FetchOpcode;
+            }
+            _ => {
+                panic!("No such instruction: {:?}", instruction);
+            }
         }
     }
 
@@ -81,20 +204,27 @@ impl Cpu {
         match self.phase {
             CpuPhase::FetchOpcode => {
                 let opcode = self.fetch8(bus);
-                self.phase = CpuPhase::InstructionDecode(CpuInstruction::from(opcode));
-            }
-            CpuPhase::InstructionDecode(CpuInstruction::LdRImm8(register)) => {
-                let value = self.fetch8(bus);
-                match register {
-                    CpuRegister::A => self.registers.a = value,
-                    CpuRegister::B => self.registers.b = value,
-                    CpuRegister::C => self.registers.c = value,
-                    CpuRegister::D => self.registers.d = value,
-                    CpuRegister::E => self.registers.e = value,
-                    CpuRegister::H => self.registers.h = value,
-                    CpuRegister::L => self.registers.l = value,
+
+                let instruction = CpuInstruction::from(opcode);
+                match instruction {
+                    CpuInstruction::LdRImm8(_) => {
+                        self.phase = CpuPhase::InstructionDecode(instruction);
+                    }
+                    CpuInstruction::AddAImm8 => {
+                        self.phase = CpuPhase::InstructionDecode(instruction);
+                    }
+                    CpuInstruction::AddAR(register) => {
+                        // only 1 m-cycle instruction, so we can directly decode it
+                        let value = self.registers.get(register);
+                        let (result, flags) = self.registers.a.cpu_add(value);
+                        self.registers.a = result;
+                        self.registers.f = flags;
+                        self.phase = CpuPhase::FetchOpcode;
+                    }
                 }
-                self.phase = CpuPhase::FetchOpcode;
+            }
+            CpuPhase::InstructionDecode(instruction) => {
+                self.decode_instruction(instruction, bus);
             }
         }
     }
--- crates/purremu-gb-core/src/cpu/tests.rs
+++ crates/purremu-gb-core/src/cpu/tests.rs
@@ -3,7 +3,7 @@ use std::hash::RandomState;
 use rand::RngExt;
 use strum::IntoEnumIterator;
 
-use crate::cpu::{Cpu, CpuInstruction, CpuPhase, CpuRegister, CpuRegisters};
+use crate::cpu::{Cpu, CpuArithmetic, CpuInstruction, CpuPhase, CpuRegister, CpuRegisters};
 use crate::memory_bus::MemoryBus;
 
 impl From<CpuInstruction> for u8 {
@@ -16,6 +16,14 @@ impl From<CpuInstruction> for u8 {
             CpuInstruction::LdRImm8(CpuRegister::E) => 0x1E,
             CpuInstruction::LdRImm8(CpuRegister::H) => 0x26,
             CpuInstruction::LdRImm8(CpuRegister::L) => 0x2E,
+            CpuInstruction::AddAImm8 => 0xC6,
+            CpuInstruction::AddAR(CpuRegister::A) => 0x87,
+            CpuInstruction::AddAR(CpuRegister::B) => 0x80,
+            CpuInstruction::AddAR(CpuRegister::C) => 0x81,
+            CpuInstruction::AddAR(CpuRegister::D) => 0x82,
+            CpuInstruction::AddAR(CpuRegister::E) => 0x83,
+            CpuInstruction::AddAR(CpuRegister::H) => 0x84,
+            CpuInstruction::AddAR(CpuRegister::L) => 0x85,
         }
     }
 }
@@ -41,7 +49,6 @@ fn test_ld_r_imm8() {
         assert_eq!(cpu.registers.pc, 0x0001);
 
         cpu.step_cycle(&bus);
-        assert_eq!(cpu.phase, CpuPhase::FetchOpcode);
         match register {
             CpuRegister::A => assert_eq!(cpu.registers.a, expected_register_value),
             CpuRegister::B => assert_eq!(cpu.registers.b, expected_register_value),
@@ -53,5 +60,110 @@ fn test_ld_r_imm8() {
         }
         assert_eq!(cpu.registers.pc, 0x0002);
         assert_eq!(cpu.phase, CpuPhase::FetchOpcode);
+    }
+}
+
+#[test]
+fn test_cpu_add() {
+    // normal add
+    let a = 0x10u8;
+    let b = 0x20u8;
+    let (result, flags) = a.cpu_add(b);
+    assert_eq!(result, 0x30);
+    assert_eq!(flags.zero, false);
+    assert_eq!(flags.subtract, false);
+    assert_eq!(flags.half_carry, false);
+    assert_eq!(flags.carry, false);
+
+    // add with half carry
+    let a = 0x0Fu8;
+    let b = 0x01u8;
+    let (result, flags) = a.cpu_add(b);
+    assert_eq!(result, 0x10);
+    assert_eq!(flags.zero, false);
+    assert_eq!(flags.subtract, false);
+    assert_eq!(flags.half_carry, true);
+    assert_eq!(flags.carry, false);
+
+    // add with carry but no zero
+    let a = 0xFFu8;
+    let b = 0x02u8;
+    let (result, flags) = a.cpu_add(b);
+    assert_eq!(result, 0x01);
+    assert_eq!(flags.zero, false);
+    assert_eq!(flags.subtract, false);
+    assert_eq!(flags.half_carry, true);
+    assert_eq!(flags.carry, true);
+
+    // add with zero result
+    let a = 0xFFu8;
+    let b = 0x01u8;
+    let (result, flags) = a.cpu_add(b);
+    assert_eq!(result, 0x00);
+    assert_eq!(flags.zero, true);
+    assert_eq!(flags.subtract, false);
+    assert_eq!(flags.half_carry, true);
+    assert_eq!(flags.carry, true);
+}
+
+#[test]
+fn test_add_a_imm8() {
+    let mut bus = MemoryBus::new();
+    let initial_a_value = 0x10;
+    let imm_value = 0x20;
+    bus.rom[0x0000] = CpuInstruction::AddAImm8.into(); // ADD A, imm8
+    bus.rom[0x0001] = imm_value;
+    let mut cpu = Cpu::new();
+    cpu.registers.set(CpuRegister::A, initial_a_value);
+
+    cpu.step_cycle(&bus);
+    assert_eq!(
+        cpu.phase,
+        CpuPhase::InstructionDecode(CpuInstruction::AddAImm8),
+        "test failed for ADD A, imm8"
+    );
+    assert_eq!(cpu.registers.pc, 0x0001);
+
+    cpu.step_cycle(&bus);
+    assert_eq!(cpu.phase, CpuPhase::FetchOpcode);
+    assert_eq!(
+        cpu.registers.a,
+        initial_a_value.overflowing_add(imm_value).0
+    );
+    assert_eq!(cpu.registers.pc, 0x0002);
+}
+
+#[test]
+fn test_add_a_r() {
+    for register in CpuRegister::iter() {
+        let mut bus = MemoryBus::new();
+        let initial_a_value = rand::random_range(u8::MIN..=u8::MAX);
+        let r_value = if register == CpuRegister::A { // if register is A, use the same value as initial_a_value to test adding A to itself
+            initial_a_value
+        } else {
+            rand::random_range(u8::MIN..=u8::MAX) // Random value for testing
+        };
+        bus.rom[0x0000] = CpuInstruction::AddAR(register).into(); // ADD A, r
+
+        let mut cpu = Cpu::new();
+        cpu.registers.set(CpuRegister::A, initial_a_value);
+        cpu.registers.set(register, r_value);
+        cpu.step_cycle(&bus);
+        assert_eq!(
+            cpu.phase,
+            CpuPhase::FetchOpcode,
+            "test failed for ADD A, {:?}",
+            register
+        );
+        assert_eq!(
+            cpu.registers.a,
+            initial_a_value.overflowing_add(r_value).0,
+            "register: {:?}, initial_a_value: {}, r_value: {}, result: {}",
+            register,
+            initial_a_value,
+            r_value,
+            cpu.registers.a
+        );
+        assert_eq!(cpu.registers.pc, 0x0001);
     }
 }
